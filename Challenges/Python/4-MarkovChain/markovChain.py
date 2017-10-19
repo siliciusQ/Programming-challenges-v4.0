e842ff38e12cad46ff1ad90b5df6be2a740059f6
Silicius <silicius42@gmail.com>
2017-10-19 22:38:19 +0200

    not working

--- Challenges/Python/4-MarkovChain/markovChain.py
+++ Challenges/Python/4-MarkovChain/markovChain.py
@@ -1,17 +1,42 @@
+import random
 
-def generateChain(text, chain_length=2):
-	chain = {}
+class MarkovChain:
+	def __init__(self, lengths):
+		self.lengths = []
+		self.chains = {}
+		for length in self.lengths:
+			self.chains[length] = {}
+	
+	def add_to_chain(self, key, item):
+		for length in self.lengths:
+			node = self.chains[length].get(key[-length:], {})
+			frequency = node.get(item, 0)
+			
+			node[item] = frequency+1
+			self.chains[length][key[-length:] = node
+	
+	def generate(self, level, length):
+		chain = self.chains[level]
+		generated = random.choice(chain.items())
+		while len(generated) < length:
+			biggest = {'':0}
+			for n in chain[generated[-level:]]:
+				if n[1] > biggest[1]:
+					biggest = n
+					
+
+def generateChain_fromText(text, chain_lengths):
+	chain = MarkovChain(chain_lengths)
 	buffer = ''
 	for char in text:
-		if len(buffer) < chain_length:
+		if len(buffer) < max(chain_lengths):
 			buffer += char
 			continue
-			
-		node = chain.get(buffer, {})
-		cell = node.get(char, 0)
-		
-		node[char] = cell+1
-		chain[buffer] = node
-		
-		buffer = buffer[1:] + char
+		chain.add_to_chain(buffer, char)
 	return chain
+			
+
+def generateText(chain, length):
+	text = random.choice(chain.keys())
+	while len(text) < length:
+		
