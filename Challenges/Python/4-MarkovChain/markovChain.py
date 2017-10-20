ba45328cab951d97770f9b422715fd9cca32dd38
Silicius <silicius42@gmail.com>
2017-10-20 16:53:54 +0200

    some improvments

--- Challenges/Python/4-MarkovChain/markovChain.py
+++ Challenges/Python/4-MarkovChain/markovChain.py
@@ -1,45 +1,35 @@
 import random
 
-class MarkovChain:
-	def __init__(self, level):
-		self.level = level
-		self.chain = {}
+class NGram:
+	def __init__(self, order):
+		self.order = order
+		self.ngram = {}
 	
-	def add_to(self, key, succ):
-		if len(key) < self.level:
-			raise ValueError("To short key for chain! [ {} ]".format(key))
+	def feed(self, sequence):
+		for offset in range(len(sequence)-offset):
+			key = sequence[offset:offset+self.order]
+			item = sequence[offset+self.order]
+			self.add(key, item)
+	
+	def add(self, key, item):
+		node = self.ngram.get(key, {})
+		counter = node.get(item, 0)
 		
-		node = self.chain.get(key, {})
-		cell = node.get(succ, 0)
-		node[succ] = cell+1
-		self.chain[key] = node
+		node[item] = counter + 1
+		self.ngram[key] = node
+	
+	def walk(self, steps):
+		list_= self.get_seed()
+		
 	
 	def get_seed(self):
-		return random.choice(list(self.chain))
-	
-	def get_random_from(self, key):
-		pairs = list(self.chain[key].items())
-		probs = [x[1] for x in pairs]
-		return random.choices(pairs, probs, k=1)[0][0]
-
+		list_ = self.ngram.keys()
+		return list(list_)
 		
-def generateChainFromText(text, level):
-	chain = MarkovChain(level)
-	for offset in range(len(text)-level-1):
-		key = text[offset:offset+level]
-		succ = text[offset+level+1]
-		chain.add_to(key, succ)
-	return chain
+with open('test.txt', 'r') as file:
+	text = file.read()
 
-def generateTextFromChain(chain, length, seed=None):
-	text = seed or chain.get_seed()
-	while len(text) < length:
-		text += chain.get_random_from(text[-chain.level:])
-	return text
-
-		
-if __name__ == '__main__':
-	with open('test.txt', 'r') as file:
-		chain = generateChainFromText(file.read(), 6)
-	print(generateTextFromChain(chain, 50))
+chain = NGram(2)
+chain.feed(text)
+print(chain.walk(10))
 	
